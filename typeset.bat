240575f067ffd49b2a04877869f2138c4bc42cce
radibac <38759169+Radibac@users.noreply.github.com>
2021-06-21 14:59:53 +0900

    fixed typeset.bat

--- typeset.bat
+++ typeset.bat
@@ -4,8 +4,8 @@ setlocal enabledelayedexpansion
 set index = 1
 for %%i in (text*.txt) do (
     ren %%i FILE.txt
-    platex lessonReportUnit1.tex
-    dvipdfmx lessonReportUnit1.dvi -o %%i.pdf
+    platex format.tex
+    dvipdfmx format.dvi -o %%i.pdf
     ren FILE.txt TYPED%%i
     set /a !index!=!index!+1
 )
